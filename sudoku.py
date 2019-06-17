b5d900b947c894fee4165c15558e88ba06f6eab4
mbonus <mathias_b2@hotmail.com>
2019-06-17 18:07:09 +0200

    name cleanup and NumPy style docstring

--- sudoku.py
+++ sudoku.py
@@ -1,60 +1,59 @@
 import numpy as np
 
-# Recursive backtracking sudoku solver
-
 
 def sudokusolve(sudoku):
-    def next_pair(i):
+    """
+    A Sudoku solver.
+
+    A solver that finds one of the posible solutions to a Sudoku,
+    by recursively backtracking through possible entry values.
+
+    Parameters
+    ----------
+    sudoku : numpy.array
+        An unsolved Sudoku as a numpy.array, with 0'es as empty/unknown values.
+
+    Returns
+    -------
+    numpy.array
+        A solved soduku as a numpy.array or None if it is unsolvable.
+
+    """
+
+    def next_entry(i):
         return (i // 9, i % 9)
 
     def state(sudoku, i):
         i += 1
         if i == 81:
             return sudoku
-        x, y = next_pair(i)
-        if sudoku[x, y] == 0:
+        r, c = next_entry(i)
+        if sudoku[r, c] == 0:
             return solve(sudoku, i)
         return state(sudoku, i)
 
     def solve(sudoku, i):
-        x, y = next_pair(i)
-        sudoku[x, y] += 1
-        if sudoku[x, y] > 9:
-            sudoku[x, y] = 0
+        r, c = next_entry(i)
+        sudoku[r, c] += 1
+        if sudoku[r, c] > 9:
+            sudoku[r, c] = 0
             return None
-        if not constraints(sudoku, x, y):
+        if not rules(sudoku, r, c):
             return solve(sudoku, i)
         tmp = state(sudoku, i)
         if tmp is None:
             return solve(sudoku, i)
         return sudoku
 
-    def constraints(sudoku, x, y):
+    def rules(sudoku, r, c):
         def row(sudoku, row):
             return unique(sudoku[row, :])
 
         def column(sudoku, column):
             return unique(sudoku[:, column])
 
-        def box(sudoku, x, y):
-            return unique(sudoku[x//3*3:x//3*3+3, y//3*3:y//3*3+3].flatten())
-
-        # def unique(collection):
-        #     for c in collection:
-        #         if (c != 0 and np.count_nonzero(collection == c) > 1):
-        #             return False
-        #     return True
-
-        # def unique(collection):
-        #     for i in range(9):
-        #         for j in range(i):
-        #             if collection[i] != 0 and collection[i] == collection[j]:
-        #                 return False
-        #     return True
-
-        # def unique(collection):
-        #     collection = collection[collection != 0]
-        #     return len(set(collection)) == len(collection)
+        def box(sudoku, r, c):
+            return unique(sudoku[r//3*3:r//3*3+3, c//3*3:c//3*3+3].flatten())
 
         def unique(collection):
             mem = []
@@ -64,7 +63,7 @@ def sudokusolve(sudoku):
                 mem.append(number)
             return True
 
-        return (row(sudoku, x) and column(sudoku, y) and box(sudoku, x, y))
+        return (row(sudoku, r) and column(sudoku, c) and box(sudoku, r, c))
 
     return state(sudoku, -1)
 
@@ -79,5 +78,5 @@ if __name__ == "__main__":
         [7, 0, 0, 0, 2, 0, 0, 0, 6],
         [0, 6, 0, 0, 0, 0, 2, 8, 0],
         [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]])
+        [0, 0, 0, 0, 8, 0, 0, 7, 0]])
     print(sudokusolve(s))
